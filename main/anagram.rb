48d8d12bf3739a69451953251e5c7ee7866c3338
elenadm <elena.an.dmitrieva@gmail.com>
2014-05-27 16:27:36 +0300

    Implementation of anagrams.

--- main/anagram.rb
+++ main/anagram.rb
@@ -2,7 +2,7 @@ module Anagram
   extend self
 
   def anagrams(str)
-    Hash[str.split(/\W+/).group_by { |w| w.to_s.downcase.split(//).sort }].values
+    Hash[str.split(/\W+/).group_by { |w| w.downcase.split(//).sort }].values
   end
 
 end
