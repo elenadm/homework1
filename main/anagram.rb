1ef76007dfa9856bd2f787ec46157d7ea31a6492
elenadm <elena.an.dmitrieva@gmail.com>
2014-05-27 12:17:12 +0300

    Refactoring anagram.

--- main/anagram.rb
+++ main/anagram.rb
@@ -2,16 +2,9 @@ module Anagram
   extend self
 
   def anagrams(str)
-    input = Hash[str.split(/\W+/).group_by { |w| w }].collect { |k, v| [k.to_s.split(//).sort, v] }
-    res = Hash.new
-    input.each do |key, v|
-      if !res.has_key?(key)
-        res[key] = v
-      else
-        res[key] = res[key].push(v[0])
-      end
-    end
+    res = Hash[str.split(/\W+/).group_by { |w| w.to_s.downcase.split(//).sort }]
     res.values
   end
+
 end
 
